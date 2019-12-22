5da7202fb45875e73d43ba2830862dbfafc12201
Emma Wedekind <emma.wedekind@logmein.com>
2019-12-22 11:09:28 +0100

    Removing images from config

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,13 +9,6 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
         name: `pages`,
         path: `${__dirname}/src/pages`,
       },
